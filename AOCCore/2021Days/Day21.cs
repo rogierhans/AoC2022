7157aa3d26b9dc50f7200319939caeb34745f90c
rogierhans <rogierhans@gmail.com>
2021-12-21 09:52:28 +0100

    Sjoerd trick

--- AOCCore/2021Days/Day21.cs
+++ AOCCore/2021Days/Day21.cs
@@ -22,6 +22,7 @@ class Day21 : Day
         Lines.Print("\n");
         var pos1 = Lines.First().Pattern("Player 1 starting position: {0}", int.Parse);
         var pos2 = Lines[1].Pattern("Player 2 starting position: {0}", int.Parse);
+
         var x = CalcWhoWins(pos1 - 1, pos2 - 1, 1, 0, 0);
         Console.WriteLine(x.Item1);
         Console.WriteLine(x.Item2);
@@ -40,25 +41,26 @@ class Day21 : Day
         }
         ulong wins1Total = 0;
         ulong wins2Total = 0;
-        for (int d1 = 1; d1 <= 3; d1++)
-            for (int d2 = 1; d2 <= 3; d2++)
-                for (int d3 = 1; d3 <= 3; d3++)
-                    if (turnPlayer1 == 1)
-                    {
-                        var Qpos1 = (pos1 + d1 + d3 + d2) % 10;
-                        var Qscore1 = score1 + Qpos1 + 1;
-                        var (Qwins1, Qwins2) = CalcWhoWins(Qpos1, pos2, 1 - turnPlayer1, Qscore1, score2);
-                        wins1Total += Qwins1;
-                        wins2Total += Qwins2;
-                    }
-                    else
-                    {
-                        var Qpos2 = (pos2 + d1 + d3 + d2) % 10;
-                        var Qscore2 = score2 + Qpos2 + 1;
-                        var (Qwins1, Qwins2) = CalcWhoWins(pos1, Qpos2, 1 - turnPlayer1, score1, Qscore2);
-                        wins1Total += Qwins1;
-                        wins2Total += Qwins2;
-                    }
+
+        foreach (var (sum, count) in new List<(int, ulong)>() { (3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1) })
+
+            if (turnPlayer1 == 1)
+            {
+                var Qpos1 = (pos1 + sum) % 10;
+                var Qscore1 = score1 + Qpos1 + 1;
+                var (Qwins1, Qwins2) = CalcWhoWins(Qpos1, pos2, 1 - turnPlayer1, Qscore1, score2);
+                wins1Total += Qwins1 * count;
+                wins2Total += Qwins2 * count;
+            }
+            else
+            {
+                var Qpos2 = (pos2 + sum) % 10;
+                var Qscore2 = score2 + Qpos2 + 1;
+                var (Qwins1, Qwins2) = CalcWhoWins(pos1, Qpos2, 1 - turnPlayer1, score1, Qscore2);
+                wins1Total += Qwins1 * count;
+                wins2Total += Qwins2 * count;
+            }
+
         StateMem[pos1, pos2, score1, score2, turnPlayer1] = (wins1Total, wins2Total);
         return StateMem[pos1, pos2, score1, score2, turnPlayer1];
     }
