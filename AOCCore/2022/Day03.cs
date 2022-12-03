4c8eae989267933f8697ed3283183cfc693dbe53
rogierhans <rogierhans@gmail.com>
2022-12-03 06:26:37 +0100

    Iets mooier

--- AOCCore/2022/Day03.cs
+++ AOCCore/2022/Day03.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection.Metadata;
 using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
@@ -54,35 +55,20 @@ class Day03 : Day
         int sum = 0;
         for (int k = 0; k < Lines.Count - 2; k += 3)
         {
-            var line = Lines[k];
-            // line.P();
-            HashSet<string> set = new HashSet<string>();
-            HashSet<string> set2 = new HashSet<string>();
-            HashSet<string> set3 = new HashSet<string>();
-            for (int i = 0; i < line.Length; i++)
-            {
-                set.Add(Lines[k][i].ToString());
-            }
-            for (int i = 0; i < Lines[k + 1].Length; i++)
-            {
-                set2.Add(Lines[k + 1][i].ToString());
-            }
-            for (int i = 0; i < Lines[k + 2].Length; i++)
-            {
-                set3.Add(Lines[k + 2][i].ToString());
-            }
-            char element = set.Intersect(set2).Intersect(set3).First()[0];
+            var line1 = Lines[k].List();
+            var line2 = Lines[k + 1].List();
+            var line3 = Lines[k + 2].List();
+            
+            char element = line1.Intersect(line2).Intersect(line3).First()[0];
 
             if (element >= 'a' && element <= 'z')
             {
-                sum += (int)element - 'a' + 1;
+                sum += element - 'a' + 1;
             }
             if (element >= 'A' && element <= 'Z')
             {
-                sum += (int)element - 'A' + 27;
+                sum += element - 'A' + 27;
             }
-
-            //set.ToList().Print(" ");
         }
         Console.WriteLine(sum);
 
