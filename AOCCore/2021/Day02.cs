f9f58cb2b2cd8ef419c4dd2d1a8a424e32bf5077
rogierhans <rogierhans@gmail.com>
2022-12-04 06:33:21 +0100

    lil rmove

--- AOCCore/2021/Day02.cs
+++ AOCCore/2021/Day02.cs
@@ -1,7 +1,8 @@
 ﻿//using System;
 //using System.Collections.Generic;
 //using System.Linq;
-//using System.Security.Cryptography.X509Certificates;
+//using
+//;
 //using System.Text;
 //using System.Threading.Tasks;
 
--- AOCCore/2021/Day09.cs
+++ AOCCore/2021/Day09.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Metadata;
-using System.Security.Cryptography.X509Certificates;
+
+
 using System.Text;
 using System.Threading.Tasks;
 using AOCCore;
