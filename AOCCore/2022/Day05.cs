e435ad31351cd343bf5c1bb977ffb0e9149ca1f8
rogierhans <rogierhans@gmail.com>
2022-12-05 12:04:06 +0100

    begin quotation Improved end quotation

--- AOCCore/2022/Day05.cs
+++ AOCCore/2022/Day05.cs
@@ -15,89 +15,68 @@ class Day05 : Day
 
     }
 
+
+    //Tried to improve work and failed
     public override void Part1(List<string> Lines)
     {
         TryParse(Lines);
-        var Rows = Blocks[0];
-        int max = (Lines.First().Count()+1)/4;
-        List<Stack<string>> stacks = new List<Stack<string>>();
-        for (int i = 0; i < max; i++)
+        Blocks[0].Reverse();
+        Stack<string>[] stacks = new Stack<string>[(Lines.First().Count() + 1) / 4];
+        for (int i = 0; i < stacks.Length; i++)
         {
-            stacks.Add(new Stack<string>());
+            stacks[i] = new Stack<string>();
         }
-        for (int i = Rows.Count - 2; i >= 0; i--)
+        for (int i = 1; i < Blocks[0].Count; i++)
         {
-            for (int stack = 0; stack < Rows[i].Length; stack+=4)
+            for (int stack = 0; stack < Blocks[0][i].Length; stack+=4)
             {
-                if (Rows[i][stack+1] != ' ')
+                if (Blocks[0][i][stack+1] != ' ')
                 {
-                    stacks[stack/4].Push(Rows[i][stack+1].ToString());
+                    stacks[stack/4].Push(Blocks[0][i][stack+1].ToString());
                 }
             }
         }
-
-        foreach (var line in Blocks[1])
+        foreach (var line in Blocks[1].Where(x => x!= ""))
         {
-
-            if (line == "") continue;
             var numbers = GetNumbers(line);
-            int amount = numbers[0];
-            int index1 = numbers[1] - 1;
-            int index2 = numbers[2] - 1;
-            List<string> Pops = new List<string>();   
-            for (int i = 0; i < amount; i++)
+            for (int i = 0; i < numbers[0]; i++)
             {
-               Pops.Add(stacks[index1].Pop());
-
+                stacks[numbers[2] - 1].Push(stacks[numbers[1] - 1].Pop());
             }
-            for (int i = 0; i < Pops.Count; i++)
-            {
-                stacks[index2].Push(Pops[i]);
-            }
-        }
-        stacks.ToList().Select(x => x.ToList().First()).ToList().Print();
+        };
+        stacks.ToList().Select(x => x.ToList().Last()).ToList().Print();
     }
     public override void Part2(List<string> Lines)
     {
         TryParse(Lines);
-        var Rows = Blocks[0];
-        int max = (Lines.First().Count() + 1) / 4;
-        List<Stack<string>> stacks = new List<Stack<string>>();
-        for (int i = 0; i < max; i++)
+        Blocks[0].Reverse();
+        Stack<string>[] stacks = new Stack<string>[(Lines.First().Count() + 1) / 4];
+        for (int i = 0; i < stacks.Length; i++)
         {
-            stacks.Add(new Stack<string>());
+            stacks[i] = new Stack<string>();
         }
-        for (int i = Rows.Count - 2; i >= 0; i--)
+        for (int i = 1; i < Blocks[0].Count; i++)
         {
-            for (int stack = 0; stack < Rows[i].Length; stack += 4)
+            for (int stack = 0; stack < Blocks[0][i].Length; stack += 4)
             {
-                if (Rows[i][stack + 1] != ' ')
+                if (Blocks[0][i][stack + 1] != ' ')
                 {
-                    stacks[stack / 4].Push(Rows[i][stack + 1].ToString());
+                    stacks[stack / 4].Push(Blocks[0][i][stack + 1].ToString());
                 }
             }
         }
 
-
-        foreach (var line in Blocks[1])
+        foreach (var line in Blocks[1].Where(x => x != ""))
         {
-
-            if (line == "") continue;
             var numbers = GetNumbers(line);
-            int amount = numbers[0];
-            int index1 = numbers[1] - 1;
-            int index2 = numbers[2] - 1;
             List<string> Pops = new List<string>();
-            for (int i = 0; i < amount; i++)
+            for (int i = 0; i < numbers[0]; i++)
             {
-                Pops.Add(stacks[index1].Pop());
-
+                Pops.Add(stacks[numbers[1] - 1].Pop());
             }
-            for (int i = Pops.Count - 1; i >= 0; i--)
-            {
-                stacks[index2].Push(Pops[i]);
-            }
+            Pops.Reverse();
+            Pops.ForEach(x => stacks[numbers[2] - 1].Push(x));
         }
-        stacks.ToList().Select(x => x.ToList().First()).ToList().Print();
+        stacks.ToList().Select(x => x.ToList().Last()).ToList().Print();
     }
 }
