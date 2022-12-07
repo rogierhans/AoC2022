a748976f195f3cfbd56d155391cc8b8ec6e4e3c6
rogierhans <rogierhans@gmail.com>
2022-12-07 21:42:47 +0100

    Street Magic alpha 0.000001

--- AOCCore/2022/Day06.cs
+++ AOCCore/2022/Day06.cs
@@ -1,11 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Headers;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace AOCCore._2022;
+﻿namespace AOCCore._2022;
 
 class Day06 : Day
 {
---
+++ AOCCore/StreetMagic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AOCCore
+{
+    class StreetMagic
+    {
+        public StreetMagic(List<string> lines)
+        {
+            FindPattern(lines);
+        }
+
+        public List<List<(string, string)>> FindPattern(List<string> lines, string after = "")
+        {
+            List<List<(string, string)>> patterns = new();
+            int maxStringSize = lines.Max(x => x.Length);
+            for (int i = 0; i < lines.Count; i++)
+            {
+                var line = lines[i];
+                for (int length = line.Length; length >= 0; length--)
+                {
+
+                }
+                for (int j = 0; j < lines.Count; j++)
+                {
+                    if (i == j) continue; 
+                }
+                //for (int length = 1; length < maxStringSize; length++)
+                //{
+
+                //}
+            }
+
+            return patterns;
+        }
+    }
+}
