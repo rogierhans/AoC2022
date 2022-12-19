33d0b1b8eb803eed37d8304d8fa3d806c3bfec6f
rogierhans <rogierhans@gmail.com>
2022-12-19 20:49:59 +0100

    slightlyCleaner

--- AOCCore/2022/Day19.cs
+++ AOCCore/2022/Day19.cs
@@ -19,93 +19,40 @@ class Day19 : Day
         var BluePrints = new List<List<(int, int, int, int, int)>>();
         foreach (var line in Lines)
         {
-            var sadmksamsd = line.Split(".").Select(x => x.Replace("Each", "")
+            var trimmedLine = line.Split(".").Select(x => x.Replace("Each", "")
             .Replace("robot", "")
             .Replace("costs", "")
-            .Replace("Blueprint 1: ", "")
             .Replace("Each", "")).ToList();
-            //input.Print("\n");
             for (int i = 0; i < 100; i++)
             {
-                sadmksamsd = sadmksamsd.Select(x => x.Replace("Blueprint " + i + ": ", " ").Replace("  ", " ")).ToList();
+                trimmedLine = trimmedLine.Select(x => x.Replace("Blueprint " + i + ": ", " ").Replace("  ", " ")).ToList();
             }
-            //sadmksamsd.Print("\n");
-
             var ingredients = new List<(int, int, int, int, int)>();
             for (int i = 0; i < 4; i++)
             {
-                var thing = sadmksamsd[i];
-                if ("" == thing) continue;
+                var thing = trimmedLine[i];
                 var input = thing.Split(' ');
-                //  thing.P();
                 var name = input[1];
-                int number1 = int.Parse(input[2]);
-                string name1 = input[3];
                 int oreNumber = 0;
                 int clayNumber = 0;
                 int obsNumber = 0;
                 int geoNumber = 0;
-                if (name1 == "ore")
-                {
-                    oreNumber += number1;
-                }
-                if (name1 == "clay")
-                {
-                    clayNumber += number1;
-                }
-                if (name1 == "obsidian")
-                {
-                    obsNumber += number1;
-                }
-                if (name1 == "geode")
-                {
-                    geoNumber += number1;
-                }
+                if (input[3] == "ore") oreNumber += int.Parse(input[2]);
+                if (input[3] == "clay") clayNumber += int.Parse(input[2]);
+                if (input[3] == "obsidian") obsNumber += int.Parse(input[2]);
+                if (input[3] == "geode") geoNumber += int.Parse(input[2]);
                 if (thing.Contains("and"))
-
                 {
-                    number1 = int.Parse(input[5]);
-                    name1 = input[6];
-                    if (name1 == "ore")
-                    {
-                        oreNumber += number1;
-                    }
-                    if (name1 == "clay")
-                    {
-                        clayNumber += number1;
-                    }
-                    if (name1 == "obsidian")
-                    {
-                        obsNumber += number1;
-                    }
-                    if (name1 == "geode")
-                    {
-                        geoNumber += number1;
-                    }
+                    if (input[6] == "ore") oreNumber += int.Parse(input[5]);
+                    if (input[6] == "clay") clayNumber += int.Parse(input[5]);
+                    if (input[6] == "obsidian") obsNumber += int.Parse(input[5]);
+                    if (input[6] == "geode") geoNumber += int.Parse(input[5]);
                 }
                 ingredients.Add((i, oreNumber, clayNumber, obsNumber, geoNumber));
 
             }
-            ingredients.Print(" ");
             BluePrints.Add(ingredients);
         }
-
-        //int sum = 0;
-        //for (int i = 0; i < BluePrints.Count; i++)
-        //{
-        //    var BluePrint = BluePrints[i];
-        //    // var list = BluePrint.Select(x => x).ToList();
-        //    // list.Reverse();
-        //    var sets = new HashSet<(int, int, int, int, int, int, int, int)>();
-        //    sets.Add((0, 0, 0, 0, 1, 0, 0, 0));
-        //    var number = Calc(1, sets, BluePrint, (6, 23, 23, 200));
-        //    number.P();
-        //    sum += number * (i+1);
-
-        //}
-
-        //sum.P();
-
         int sum = 1;
         foreach (var BluePrint in BluePrints.Take(3))
         {
@@ -113,7 +60,8 @@ class Day19 : Day
             // list.Reverse();
             var sets = new HashSet<(int, int, int, int, int, int, int, int)>();
             sets.Add((0, 0, 0, 0, 1, 0, 0, 0));
-            var number = Calc(1, sets, BluePrint, (6, 22, 22, 200));
+            var maxMaxine = (BluePrint.Max(x => x.Item2), BluePrint.Max(x => x.Item3), BluePrint.Max(x => x.Item4), 200);
+            var number = Calc(1, sets, BluePrint, (6, 22, 22, 200), maxMaxine);
             number.P();
             sum *= number;
 
@@ -122,10 +70,11 @@ class Day19 : Day
         sum.P();
     }
 
-    public int Calc(int min, HashSet<(int, int, int, int, int, int, int, int)> set, List<(int, int, int, int, int)> BluePrint, (int, int, int, int) TrimValues)
+    public int Calc(int min, HashSet<(int, int, int, int, int, int, int, int)> set, List<(int, int, int, int, int)> BluePrint, (int, int, int, int) TrimValues, (int, int, int, int) TrimMachine)
     {
         if (min == 33)
         {
+            set.Count.P();
             set.MaxItem(x => x.Item4).P();
             return set.Max(x => x.Item4);
         }
@@ -143,21 +92,26 @@ class Day19 : Day
                 if (s1 - c1 >= 0 && s2 - c2 >= 0 && s3 - c3 >= 0 && s4 - c4 >= 0)
                 {
                     var (t1, t2, t3, t4) = TrimValues;
-                    newSet.Add(Trim(PayAndUpdate(state, BluePrint[index]), TrimValues));
+                    if (index == 0 && m1 < TrimMachine.Item1) { newSet.Add(Trim(PayAndUpdate(state, BluePrint[index]), TrimValues, TrimMachine)); }
+                    if (index == 1 && m2 < TrimMachine.Item2) { newSet.Add(Trim(PayAndUpdate(state, BluePrint[index]), TrimValues, TrimMachine)); }
+                    if (index == 2 && m3 < TrimMachine.Item3) { newSet.Add(Trim(PayAndUpdate(state, BluePrint[index]), TrimValues, TrimMachine)); }
+                    if (index == 3) { newSet.Add(Trim(PayAndUpdate(state, BluePrint[index]), TrimValues, TrimMachine)); }
+
                 }
             }
             {
-                newSet.Add(Trim(Update(state), TrimValues));
+                newSet.Add(Trim(Update(state), TrimValues, TrimMachine));
             }
         }
 
-        return Calc(min + 1, newSet, BluePrint, TrimValues);
+        return Calc(min + 1, newSet, BluePrint, TrimValues, TrimMachine);
     }
-    public (int, int, int, int, int, int, int, int) Trim((int, int, int, int, int, int, int, int) state, (int, int, int, int) TrimValues)
+    public (int, int, int, int, int, int, int, int) Trim((int, int, int, int, int, int, int, int) state, (int, int, int, int) TrimValues, (int, int, int, int) TrimMachine)
     {
         var (s1, s2, s3, s4, m1, m2, m3, m4) = state;
         var (t1, t2, t3, t4) = TrimValues;
-        return (s1.M(t1), s2.M(t2), s3.M(t3), s4.M(t4), m1.M(t1), m2.M(t2), m3.M(t3), m4.M(t4));
+        var (t1m, t2m, t3m, t4m) = TrimMachine;
+        return (s1.M(t1), s2.M(t2), s3.M(t3), s4.M(t4), m1.M(t1m), m2.M(t2m), m3.M(t3m), m4.M(t4m));
     }
     public (int, int, int, int, int, int, int, int) Update((int, int, int, int, int, int, int, int) state)
     {
