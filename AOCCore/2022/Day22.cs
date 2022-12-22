d19891708a1942f59e2fdacbcbc56fb280ffa522
rogierhans <rogierhans@gmail.com>
2022-12-22 10:51:17 +0100

    Little less spagetthi

--- AOCCore/2022/Day22.cs
+++ AOCCore/2022/Day22.cs
@@ -114,106 +114,93 @@ class Day22 : Day
                 else if (s.Zone == "B")
                 {
                     s.Down = (grid[Cx + 49, Cy + (x % 50)], "Left");
-                    if (s.Down.Item1.Zone != "C") { s.Down.Item1.Zone.P(); Console.ReadLine(); }
 
                 }
                 else if (s.Zone == "D")
                 {
-                    //D
                     s.Down = (grid[Fx + 49, Fy + (x % 50)], "Left");
-                    if (s.Zone != "D") { "ZoneNotCorrect".P(); Console.ReadLine(); }
-                    if (s.Down.Item1.Zone != "F") { s.Down.Item1.Zone.P(); Console.ReadLine(); }
 
                 }
                 else if (s.Zone == "F")
                 {
                     //F
                     s.Down = (grid[Bx + (x % 50), 0], "Down");
-                    if (s.Zone != "F") { "ZoneNotCorrect".P(); Console.ReadLine(); }
-                    if (s.Down.Item1.Zone != "B") { s.Down.Item1.Zone.P(); Console.ReadLine(); }
                 }
             }
         }
 
 
-        for (int NOYPLS = 0; NOYPLS < maxY; NOYPLS++)
+        for (int y = 0; y < maxY; y++)
         {
             for (int x = 0; x < maxX; x++)
             {
                 //up
-                var s = grid[x, NOYPLS];
+                var s = grid[x, y];
                 if (s.Content == " ") continue;
-                if (NOYPLS - 1 >= 0 && grid[x, NOYPLS - 1].Content != " ")
+                if (y - 1 >= 0 && grid[x, y - 1].Content != " ")
                 {
-                    s.Up = (grid[x, NOYPLS - 1], "Up");
+                    s.Up = (grid[x, y - 1], "Up");
                 }
                 else if (s.Zone == "A")
                 {
                     s.Up = (grid[0, Fy + (x % 50)], "Right");
-                    if (s.Up.Item1.Zone != "F") { s.Up.Item1.Zone.P(); Console.ReadLine(); }
 
                 }
                 else if (s.Zone == "B")
                 {
                     s.Up = (grid[Fx + x % 50, Fy + 49], "Up");
-                    if (s.Up.Item1.Zone != "F") { s.Up.Item1.Zone.P(); Console.ReadLine(); }
 
                 }
                 else if (s.Zone == "E")
                 {
                     s.Up = (grid[Cx, Cy + x], "Right");
-                    if (s.Up.Item1.Zone != "C") { s.Up.Item1.Zone.P(); Console.ReadLine(); }
                 }
             }
         }
         for (int y = 0; y < maxY; y++)
         {
-            for (int NOXPLS = 0; NOXPLS < maxX; NOXPLS++)
+            for (int x = 0; x < maxX; x++)
             {
                 //Left
-                var s = grid[NOXPLS, y];
+                var s = grid[x, y];
                 if (s.Content == " ") continue;
-                if (NOXPLS - 1 >= 0 && grid[NOXPLS - 1, y].Content != " ")
+                if (x - 1 >= 0 && grid[x - 1, y].Content != " ")
                 {
-                    s.Left = (grid[NOXPLS - 1, y], "Left");
+                    s.Left = (grid[x - 1, y], "Left");
                 }
                 else if (s.Zone == "A")
                 {
 
                     s.Left = (grid[0, Ey + (49 - (y % 50))], "Right");
-                    if (s.Left.Item1.Zone != "E") { s.Left.Item1.Zone.P(); Console.ReadLine(); }
                 }
                 else if (s.Zone == "C")
                 {
 
                     s.Left = (grid[0 + (y % 50), Ey], "Down");
-                    if (s.Left.Item1.Zone != "E") { s.Left.Item1.Zone.P(); Console.ReadLine(); }
                 }
                 else if (s.Zone == "E")
                 {
 
                     s.Left = (grid[Ax, Ay + (49 - (y % 50))], "Right");
-                    if (s.Left.Item1.Zone != "A") { s.Left.Item1.Zone.P(); Console.ReadLine(); }
                 }
                 else if (s.Zone == "F")
                 {
 
                     s.Left = (grid[Ax + (y % 50), 0], "Down");
-                    if (s.Left.Item1.Zone != "A") { s.Left.Item1.Zone.P(); Console.ReadLine(); }
                 }
             }
         }
 
         for (int y = 0; y < maxY; y++)
         {
-            for (int NOXPLS = 0; NOXPLS < maxX; NOXPLS++)
+            for (int x = 0; x < maxX; x++)
             {
                 //Right
-                var s = grid[NOXPLS, y];
+                var s = grid[x, y];
                 if (s.Content == " ") continue;
-                if (NOXPLS + 1 < maxX && grid[NOXPLS + 1, y].Content != " ")
+                if (x + 1 < maxX && grid[x + 1, y].Content != " ")
                 {
-                    s.Right = (grid[NOXPLS + 1, y], "Right");
+                    s.Right = (grid[x + 1, y], "Right");
                 }
                 else if (s.Zone == "B")
                 {
@@ -240,95 +227,16 @@ class Day22 : Day
 
             }
         }
-        //  Console.ReadLine();
-        for (int y = 0; y < maxY; y++)
-        {
-            for (int x = 0; x < maxX; x++)
-            {
-                var s = grid[x, y];
-                if (s.Content == " ") continue;
-                //   (s.x, s.y, s.Zone).P();
-                if (s.Up.Item1.Zone == "?" || s.Down.Item1.Zone == "?" || s.Left.Item1.Zone == "?" || s.Right.Item1.Zone == "?") { "NOOOOOOOOOOOOOOOOO".P(); }
-            }
-        }
+
 
 
         var instructions = GetNumbers(Blocks[1].First());
-        //  instructions.Print(" ");
         List<string> names = new List<string>() { "Right", "Down", "Left", "Up" };
-        //List<(int, int)> dir = new List<(int, int)>() { (1, 0), (0, 1), (-1, 0), (0, -1) };
-        // int dirIndex = 0;
-        int CY = 0;
-        int CX = 0;
-        for (int i = 0; i < maxX; i++)
-        {
-            if (grid[i, CY].Content == ".") { CX = i; break; }
-        }
-        var currentLocation = (grid[CX, CY], "Right");
+        var currentLocation = (grid[50, 0], "Right");
 
-        {
-            foreach (var direciotne in new List<string>() { "Up", "Down", "Right", "Left" })
-                for (int x = 0; x < maxX; x++)
-                {
-                    for (int y = 0; y < maxY; y++)
-                    {
-                        if (grid[x, y].Zone == "?") continue;
-                        currentLocation = (grid[x, y], direciotne);
-
-                        var pos = currentLocation.Item1;
-                        var dir = currentLocation.Item2;
-                        for (int i = 0; i < 400; i++)
-                        {
-                            // Print(maxX, maxY, grid, currentLocation);
-                            //  (currentLocation.Item1.x %50, currentLocation.Item1.y % 50).P();
-                            var (position, currentDirection) = currentLocation;
-                            var next = (new Cell("????", -1, -1), "faskdlsksa");
-                            if (currentDirection == "Left")
-                            {
-                                next = position.Left;
-                            }
-                            if (currentDirection == "Right")
-                            {
-                                next = position.Right;
-                            }
-                            if (currentDirection == "Up")
-                            {
-                                next = position.Up;
-                            }
-                            if (currentDirection == "Down")
-                            {
-                                next = position.Down;
-                            }
-                            if (next.Item1.Content == "????") { throw new Exception(); }
-                            // if (next.Item1.Content == ".")
-                            {
-                                currentLocation = next;
-                            }
-                        }
-                        if (pos != currentLocation.Item1)
-                        {
-                            pos.P();
-
-                        }
-                        if (dir != currentLocation.Item2)
-                        {
-                            pos.P();
-
-                        }
-                    }
-                }
-        }
-
-
+        Print(maxX, maxY, grid, currentLocation);
         foreach (var (steps, direction) in instructions)
         {
-            // Print(maxX, maxY, grid, currentLocation.Item1.x, currentLocation.Item1.y);
-            //currentLocation.P();
-            //(steps, direction).P();
-
-            //  Print(maxX, maxY, grid, currentLocation);
-            //   currentLocation.P();
-            // Console.ReadLine();
             for (int i = 0; i < steps; i++)
             {
 
@@ -374,7 +282,7 @@ class Day22 : Day
         }
         {
             int dirIndex = names.IndexOf(currentLocation.Item2);
-            currentLocation.P();
+            //Print(maxX, maxY, grid, CY, CX);
             (1000 * (currentLocation.Item1.y + 1) + 4 * (currentLocation.Item1.x + 1) + dirIndex).P();
             //((CY + 1), (CX + 1), dirIndex).P();
         }
@@ -383,32 +291,34 @@ class Day22 : Day
 
     private static void Print(int maxX, int maxY, Cell[,] grid, (Cell, string) location)
     {
-        location.P();
+        "Left:".P();
         var printGrind = new string[maxX, maxY];
 
         for (int y = 0; y < maxY; y++)
         {
             for (int x = 0; x < maxX; x++)
             {
-                if (grid[x, y].Content == "#")
+                if (grid[x, y].Content == "#"|| grid[x, y].Content == ".")
                 {
-                    printGrind[x, y] = ".";
-                }
-                else if (grid[x, y].Content == " ")
-                {
-                    printGrind[x, y] = "$";
+                    if (grid[x, y].Left.Item1.Zone == grid[x, y].Zone)
+
+                        printGrind[x, y] = "<";
+                    else
+                    {
+                        printGrind[x, y] = grid[x, y].Left.Item1.Zone;
+                    }
                 }
                 else
                     printGrind[x, y] = " ";
                 //printGrind[x, y] = grid[x, y].Content == "." ? " " : grid[x, y].Content;
                 //printGrind[x, y] = grid[x, y].Content == "#" ? "." : grid[x, y].Content;
                 // grid[x, y].Content != " " ? grid[x, y].Left.Item1.Zone : "!";
-                // printGrind[x, y] = grid[x, y].Content != " " ? grid[x, y].Down.Item2[0].ToString() : "!";
+                // printGrind[x, y] = grid[x, y].Content != " " && grid[x, y].Down.Item1.Zone. ?  : " ";
             }
         }
         printGrind[location.Item1.x, location.Item1.y] = "█";
         // printGrind.ToLists().GridSelect(x => x == " " ? "@" : x).Print("");
-        printGrind.ToLists().Select(x => x.Take(160).ToList()).ToList().Print("");
+        printGrind.ToLists().ToList().Print("");
 
         Console.ReadLine();
     }
