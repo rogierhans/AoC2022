b870617165cc9e1edc47e81bb44b7716253d58f6
rogierhans <rogierhans@gmail.com>
2022-12-23 10:30:36 +0100

    Iets beter

--- AOCCore/2022/Day23.cs
+++ AOCCore/2022/Day23.cs
@@ -17,29 +17,21 @@ class Day23 : Day
         GetInput("2022", "23");
 
     }
-
-    //If there is no Elf in the N, NE, or NW adjacent positions, the Elf proposes moving north one step.
-    //If there is no Elf in the S, SE, or SW adjacent positions, the Elf proposes moving south one step.
-    //If there is no Elf in the W, NW, or SW adjacent positions, the Elf proposes moving west one step.
-    //If there is no Elf in the E, NE, or SE adjacent positions, the Elf proposes moving east one step.
-
     class Elf
     {
         public (int, int) Next;
         public bool hasNext = false;
-        public List<List<(int, int)>> PList = new List<List<(int, int)>> {
-            new List<(int, int)>() { (-1, 0),(-1, 1) , (-1, -1), },
-            new List<(int, int)>() { (1, 0) ,(1,1)   , (1,-1) } ,
-            new List<(int, int)>() { (0, -1), (1, -1), (-1, -1) },
-            new List<(int, int)>() { (0, 1) ,(1, 1)  , (-1, 1) },
-        };
-
     }
     public override void Part1(List<string> Lines)
     {
+        List<List<(int, int)>> PList = new List<List<(int, int)>> {
+            new List<(int, int)>() { (-1, 0),(-1, 1), (-1, -1), },
+            new List<(int, int)>() { (1, 0) ,(1,1), (1,-1) } ,
+            new List<(int, int)>() { (0, -1),(1, -1), (-1, -1) },
+            new List<(int, int)>() { (0, 1) ,(1, 1)  , (-1, 1) },
+        };
         var grid = Lines.Select(x => x.List()).ToList();
         Dictionary<(int, int), Elf> elfs = new Dictionary<(int, int), Elf>();
-
         for (int r = 0; r < grid.Count; r++)
         {
             for (int c = 0; c < grid[0].Count; c++)
@@ -49,48 +41,19 @@ class Day23 : Day
                 elfs[(r, c)] = (elf);
             }
         }
+        var n8 = new List<(int, int)> { (1, 1), (-1, 1), (1, -1), (-1, -1), (1, 0), (-1, 0), (0, 1), (0, -1) };
 
         for (int k = 0; k < 10000; k++)
         {
-
-
             var Claims = new Dictionary<(int, int), int>();
-
             foreach (var ((r, c), elf) in elfs)
             {
-                bool Somebody = false;
-                foreach (var (nx, ny) in new List<(int, int)> { (1, 1), (-1, 1), (1, -1),
-                                        (-1, -1), (1, 0), (-1, 0),
-                                        (0, 1), (0, -1) })
-                {
-                    if (elfs.ContainsKey((nx +r, ny+c)))
-                        Somebody = true;
-                }
-                if (!Somebody) { //"OK".P();
-                    continue; }
-
-
+                if (n8.All(coord => !elfs.ContainsKey((coord.Item1 + r, coord.Item2 + c)))) continue;
                 for (int d = 0; d < 4; d++)
                 {
-                    bool free = true;
-
-                    foreach (var (dx, dy) in elf.PList[d])
+                    if (PList[d].All(coord => !elfs.ContainsKey((r + coord.Item1, c + coord.Item2))))
                     {
-
-                        // Console.WriteLine(offsetI +" "+ offsetJ);
-                        int nr = r + dx;
-                        int nc = c + dy;
-
-                        if (elfs.ContainsKey((nr, nc)))
-                        {
-                            free = false;
-                        }
-
-                    }
-                    if (free)
-                    {
-                        //d.P();
-                        var (dx, dy) = elf.PList[d][0];
+                        var (dx, dy) = PList[d][0];
                         elf.Next = (dx + r, dy + c);
                         elf.hasNext = true;
                         if (!Claims.ContainsKey((dx + r, dy + c)))
@@ -101,64 +64,43 @@ class Day23 : Day
                         break;
                     }
                 }
-
-
-
             }
-            foreach (var (_, elf) in elfs) {
-                var prop = elf.PList.First();
-                elf.PList.RemoveAt(0);
-                elf.PList.Add(prop);
-            }
-
+            var prop = PList.First();
+            PList.RemoveAt(0);
+            PList.Add(prop);
             var newElfs = new Dictionary<(int, int), Elf>();
-
             bool moves = false;
             foreach (var ((r, c), elf) in elfs)
             {
-                if (elf.hasNext)
+                if (elf.hasNext && Claims[elf.Next] == 1)
                 {
-                    if (Claims[elf.Next] == 1)
-                    {
-                        moves= true;    
-                        //(r, c, elf.Next).P();
-                        newElfs[elf.Next] = elf;
-                    }
-                    else {
-                        newElfs[(r, c)] = elf;
-                    }
+                    moves = true;
+                    newElfs[elf.Next] = elf;
                 }
-                else {
+                else
+                {
                     newElfs[(r, c)] = elf;
                 }
             }
-            if (!moves) {
+            if (!moves)
+            {
                 k.P();
                 break;
             }
-
-            //Print(elfs);
             elfs.ToList().ForEach(x => x.Value.hasNext = false);
             elfs = newElfs;
-           // Print(elfs);
-            //Claims.ToList().Print(" "); 
-           // Console.ReadLine();
+
         }
         Print(elfs);
-        // print.GridSelect(x => x ? 1 : 0).Print("");
         Console.ReadLine();
     }
 
     private static void Print(Dictionary<(int, int), Elf> elfs)
     {
         int maxX = elfs.Keys.Max(x => x.Item1);
-
         int minX = elfs.Keys.Min(x => x.Item1);
-
         int maxY = elfs.Keys.Max(x => x.Item2);
-
         int minY = elfs.Keys.Min(x => x.Item2);
-
         var printGrind = new string[maxX - minX + 1, maxY - minY + 1];
         for (int x = 0; x < printGrind.GetLength(0); x++)
         {
